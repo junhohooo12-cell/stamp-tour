3157868f280d9b25c7c482734ba6ceb92a73335c
junhohooo12-cell <junhohooo12@gmail.com>
2026-05-07 17:07:49 +0900

    Update supabase_add_coupon.sql

--- supabase_add_coupon.sql
+++ supabase_add_coupon.sql
@@ -1,4 +1,15 @@
--- Supabase SQL Editor에서 실행하세요
--- members 테이블에 coupon 컬럼 추가
-
-ALTER TABLE members ADD COLUMN IF NOT EXISTS coupon jsonb DEFAULT NULL;
+{
+  "version": 2,
+  "builds": [
+    {
+      "src": "index.html",
+      "use": "@vercel/static"
+    }
+  ],
+  "routes": [
+    {
+      "src": "/(.*)",
+      "dest": "/index.html"
+    }
+  ]
+}
